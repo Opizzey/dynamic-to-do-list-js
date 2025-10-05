04eed83f9466371dd59caf66e2c9dbb78af46e4d
Dennis Opiyo <dopiyoochieng@gmail.com>
2025-10-05 11:41:14 -0700

    edited .js file and work on the DOM section

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     const addButton = document.getElementById('add-task-btn');
+    
     const taskInput = document.getElementById('task-input');
+    
     const taskList = document.getElementById('task-list');
 
     function addTask() {
